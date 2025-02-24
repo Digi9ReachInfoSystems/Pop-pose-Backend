8971f26b6b84c6745b2bad3829fcd203b15a875e
Flutterneu <rsmx141@gmail.com>
2025-02-24 13:26:47 +0530

    add route for selecting a frame in user journey

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const user = require("../controllers/userController");
 
 router.post("/start", user.startUserJourney);
+router.post("/:userId/select-frame", user.selectFrame);
 
 module.exports = router;
